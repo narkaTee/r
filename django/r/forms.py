1667fe58a935fbe1df6da154b3a3a494391f1a53
rfsp <rfsp>
2014-03-11 09:47:43 +0100

    setup: path validator hinzugefügt

--- django/r/forms.py
+++ django/r/forms.py
@@ -1,8 +1,24 @@
 from splunkdj.setup import forms
+from django.core import validators
+import os
+
+
+class PathExistsValidator(object):
+    def __call__(self, value):
+        if not os.path.exists(value):
+            raise validators.ValidationError(
+                'Path does not exists'
+            )
+
+
+class PathField(forms.CharField):
+    def __init__(self, *args, **kwargs):
+        super(PathField, self).__init__(*args, **kwargs)
+        self.validators.append(PathExistsValidator())
 
 
 class SetupForm(forms.Form):
-    email = forms.CharField(
+    email = PathField(
         endpoint='configs/conf-r',
         entity='paths',
         field='r',
