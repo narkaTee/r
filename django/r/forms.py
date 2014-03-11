2a537fd76e8af6430a3be3261d76c939bc4adf6b
rfsp <rfsp>
2014-03-11 09:24:13 +0100

    library download buttons in setup view

--- django/r/forms.py
+++ django/r/forms.py
@@ -6,9 +6,5 @@ class SetupForm(forms.Form):
         endpoint='configs/conf-r',
         entity='paths',
         field='r',
-        label='Library Path',
-        help_text=r'Local path to the R library executable '
-                  r'(for example C:\Program Files\R\R-3.0.3\bin\R.exe or '
-                  r'/Library/Frameworks/R.framework/Versions/Current/Resources/bin/R). '
-                  '\nYou can download R from http://www.r-project.org/'
+        label='Please specify the library installation path:',
     )
