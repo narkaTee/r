322619fda82f68db7c2535f1e4cc59833c209257
rfsp <rfsp>
2014-03-16 21:46:21 +0100

    more generic way to iterate script stanzas

--- bin/config.py
+++ bin/config.py
@@ -3,3 +3,12 @@ from splunk.clilib import cli_common as cli
 cli.cacheConfFile('r')
 
 r_config = cli.confSettings['r']
+
+
+def iter_stanzas(scheme):
+    prefix = '%s://' % scheme
+    for stanza_name in r_config:
+        if stanza_name.startswith(prefix):
+            stanza = r_config[stanza_name]
+            after_prefix = stanza_name[len(prefix):]
+            yield stanza, after_prefix
--- bin/scripts.py
+++ bin/scripts.py
@@ -3,7 +3,7 @@ import os
 import base64
 import config
 
-script_stanza_prefix = 'script://'
+scheme = 'script'
 custom_scripts_path = path.get_named_path('scripts')
 
 
@@ -12,11 +12,9 @@ def refresh_files():
     if not os.path.exists(custom_scripts_path):
         os.makedirs(custom_scripts_path)
 
-    for stanza_name in config.r_config:
-        if stanza_name.startswith(script_stanza_prefix):
-            script_stanza = config.r_config[stanza_name]
-            script_content = base64.decodestring(script_stanza['content'])
-            script_filename = stanza_name[len(script_stanza_prefix):] + '.r'
-            script_full_path = os.path.join(custom_scripts_path, script_filename)
-            with open(script_full_path, 'wb') as f:
-                f.write(script_content)
+    for stanza, name in config.iter_stanzas(scheme):
+        script_content = base64.decodestring(stanza['content'])
+        script_filename = name + '.r'
+        script_full_path = os.path.join(custom_scripts_path, script_filename)
+        with open(script_full_path, 'wb') as f:
+            f.write(script_content)
