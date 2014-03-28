7a47fe72434b895edecc288b44bf274424d9eac1
rfsp <rfsp>
2014-03-29 00:15:20 +0100

    file locking can now be stacked

--- bin/lockfile.py
+++ bin/lockfile.py
@@ -2,12 +2,19 @@
 from contextlib import contextmanager
 import portalocker
 
+locked_files = set()
+
 @contextmanager
 def file_lock(lock_file):
-    with open(lock_file, 'w') as fd:
-        portalocker.lock(fd, portalocker.LOCK_EX)
-        try:
-            yield
-        finally:
-            portalocker.unlock(fd)
+    if lock_file in locked_files:
+        yield
+    else:
+        with open(lock_file, 'w') as fd:
+            portalocker.lock(fd, portalocker.LOCK_EX)
+            locked_files.add(lock_file)
+            try:
+                yield
+            finally:
+                portalocker.unlock(fd)
+                locked_files.remove(lock_file)
     #os.remove(lock_file)
