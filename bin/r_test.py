f0300854838de24691a40092194e4f03acf92d74
rfsp <rfsp>
2014-04-13 10:58:43 +0200

    added unit test for package usage logs

--- bin/r_test.py
+++ bin/r_test.py
@@ -22,3 +22,25 @@ class RTestCase(PathTestCase):
         ''')
         self.assertEqual(len(rows), 1)
         self.assertEqual(rows[0]['Name'], 'Robert')
+
+    def test_r_package_usage(self):
+        service = Service([
+            Stanza('package://race', {}),
+            Stanza('package://boot', {}),
+        ])
+        _, rows = r(service, None, '''
+        library(race)
+        library(boot)
+        library(race)
+        output = data.frame(h1=c('v1'))
+        ''')
+        indexed_events = [post['query']['body'] for post in service.indexes['r'].service.posts]
+        self.assertEqual(len(indexed_events), 4)
+        post = service.indexes['r'].service.posts[1]
+        body = post['query']['body']
+        self.assertTrue('action=\"package_usage\"' in body)
+        self.assertTrue('package_name=\"race\"' in body)
+        post = service.indexes['r'].service.posts[2]
+        body = post['query']['body']
+        self.assertTrue('action=\"package_usage\"' in body)
+        self.assertTrue('package_name=\"boot\"' in body)
