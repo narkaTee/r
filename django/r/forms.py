33500adfcaec040ffa3d967696512e5526b25ed6
rfsp <rfsp>
2014-05-23 08:07:29 +0200

    include improved R path error messages also in setup page

--- django/r/forms.py
+++ django/r/forms.py
@@ -1,24 +1,33 @@
-from splunkdj.setup import forms
-from django.core import validators
 import os
+import sys
+
+from django.core import validators
+
+from splunkdj.setup import forms
 
 
-class PathExistsValidator(object):
+# allow imports from bin directory
+bin_path = os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'bin')
+sys.path += bin_path
+
+import framework as frameworklib
+
+
+class RPathValidator(object):
     def __call__(self, value):
-        if not os.path.exists(value):
-            raise validators.ValidationError(
-                'Path does not exists'
-            )
+        def raise_error(msg):
+            raise validators.ValidationError(msg)
+        frameworklib.verify_r_path(value,raise_error)
 
 
-class PathField(forms.CharField):
+class RPathField(forms.CharField):
     def __init__(self, *args, **kwargs):
-        super(PathField, self).__init__(*args, **kwargs)
-        self.validators.append(PathExistsValidator())
+        super(RPathField, self).__init__(*args, **kwargs)
+        self.validators.append(RPathValidator())
 
 
 class SetupForm(forms.Form):
-    r_path = PathField(
+    r_path = RPathField(
         endpoint='configs/conf-r',
         entity='paths',
         field='r',
